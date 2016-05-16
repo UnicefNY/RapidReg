8901886c4b629514bef7542f5e654a308b680a97
Chen Yang <chen.yang@rea-group.com>
2016-05-16 17:39:21 +0800

    feature/login--implement login offline & refector

--- app/src/main/java/org/unicef/rapidreg/model/User.java
+++ app/src/main/java/org/unicef/rapidreg/model/User.java
@@ -47,4 +47,76 @@ public class User {
         this.unauthenticatedPassword = unauthenticatedPassword;
         this.language = language;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getDbKey() {
+        return dbKey;
+    }
+
+    public void setDbKey(String dbKey) {
+        this.dbKey = dbKey;
+    }
+
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(String organisation) {
+        this.organisation = organisation;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUnauthenticatedPassword() {
+        return unauthenticatedPassword;
+    }
+
+    public void setUnauthenticatedPassword(String unauthenticatedPassword) {
+        this.unauthenticatedPassword = unauthenticatedPassword;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
