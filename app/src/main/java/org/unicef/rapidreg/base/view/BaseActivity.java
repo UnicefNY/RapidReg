0f5dfa2304bf3f5e847279eb0b7b6bb6e28ac985
Chen Yang <chen.yang@rea-group.com>
2016-05-19 16:14:07 +0800

    feature/cases--add menu & toolbar & casesActivity

--- app/src/main/java/org/unicef/rapidreg/base/view/BaseActivity.java
+++ app/src/main/java/org/unicef/rapidreg/base/view/BaseActivity.java
@@ -1,6 +1,28 @@
 package org.unicef.rapidreg.base.view;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 
-public class BaseActivity extends AppCompatActivity {
+import org.unicef.rapidreg.R;
+
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
 }
