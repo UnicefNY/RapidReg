7d9c319590d385a0203eb7799db28db6f75abed4
Chen Yang <chen.yang@rea-group.com>
2016-05-19 09:21:13 +0800

    feature/cases--add CasesActivity & CasesListFragment

---
+++ app/src/main/java/org/unicef/rapidreg/base/view/BaseActivity.java
@@ -0,0 +1,6 @@
+package org.unicef.rapidreg.base.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/org/unicef/rapidreg/cases/CasesListFragment.java
@@ -0,0 +1,19 @@
+package org.unicef.rapidreg.cases;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.unicef.rapidreg.R;
+
+public class CasesListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cases_list, container, false);
+    }
+}
