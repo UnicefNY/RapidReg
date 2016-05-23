a854838a89bf7c3c40ffa6318aeadcfc2757496f
shane51 <suse51@163.com>
2016-05-23 14:34:01 +0800

    feature/login -- Add login input box format validate

--- app/src/test/java/org/unicef/rapidreg/login/LoginActivityTest.java
+++ app/src/test/java/org/unicef/rapidreg/login/LoginActivityTest.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.maven.artifact.ant.shaded.StringUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,10 +46,67 @@ public class LoginActivityTest {
         loginButton = (Button) activity.findViewById(R.id.button_login);
         urlEditview = (EditText) activity.findViewById(R.id.editview_url);
     }
-
+//    @Test
+//    public void correctUserandPasswordShouldLoginSuccess() {
+//        usernameEditview.setText("primero");
+//        passwordEditview.setText("qu01n23!");
+//        if (urlEditview != null)
+//            urlEditview.setText("http://10.29.3.184:3000");
+//
+//        loginButton.performClick();
+//        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+//        assertThat("Next activity has started", application.getNextStartedActivity(), is(notNullValue()));
+//    }
+//
+//
+//    @Test
+//    public void emptyUserAndPasswordShouldShowError() {
+//        loginButton.performClick();
+//
+//        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+//        assertThat("Next activity should not started", application.getNextStartedActivity(), is(nullValue()));
+//        assertThat("Show error for user field ", usernameEditview.getError(), is(CoreMatchers.notNullValue()));
+//        assertThat("Show error for Password field ", passwordEditview.getError(), is(CoreMatchers.notNullValue()));
+//    }
 
     @Test
     public void invalidUserNameFormatShouldShowError() {
+
+        Map<String,String> nameList = new HashMap<String,String>();
+        nameList.put("NAME_CONTAINS_SPECAIL","primer@/?");
+        nameList.put("NAME_TOO_LONG",StringUtils.leftPad("", 255, "a"));
+
+        assertAllInvalidUserNameFormatShouldFail(nameList);
+
+
+
+    }
+
+//    @Test
+//    public void invalidAccountShouldLoginFail() {
+//        usernameEditview.setText("invalidUserName");
+//        passwordEditview.setText("invalidPassword");
+//        if (urlEditview == null)
+//            urlEditview.setText("http://10.29.3.184:3000");
+//
+//        loginButton.performClick();
+//        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+//        assertThat("Next activity should not started", application.getNextStartedActivity(), is(nullValue()));
+//    }
+
+    private void assertAllInvalidUserNameFormatShouldFail(Map<String, String> nameList) {
+        for (Map.Entry<String, String> nameEntry : nameList.entrySet())
+        {
+            usernameEditview.setText(nameEntry.getValue());
+            passwordEditview.setText("qu01n23!");
+            if (urlEditview == null)
+                urlEditview.setText("http://10.29.3.184:3000");
+
+            loginButton.performClick();
+            ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+            assertThat("Next activity should not started", application.getNextStartedActivity(), is(nullValue()));
+            assertThat("Show error for " + nameEntry.getKey(), usernameEditview.getError(), is(CoreMatchers.notNullValue()));
+        }
 
     }
 }
