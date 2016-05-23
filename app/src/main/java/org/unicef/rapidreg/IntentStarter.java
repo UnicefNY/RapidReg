7f2763b9c712a516f9f23a7eec6689377976680e
Chen Yang <chen.yang@rea-group.com>
2016-05-23 11:58:42 +0800

    feature/login#33--implement toolbar

---
+++ app/src/main/java/org/unicef/rapidreg/IntentStarter.java
@@ -0,0 +1,13 @@
+package org.unicef.rapidreg;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class IntentStarter {
+
+    public void showSearch(Activity context) {
+//        Intent intent = new Intent(context, SearchActivity.class);
+//        context.startActivity(intent);
+//        context.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+}
--- app/src/main/java/org/unicef/rapidreg/login/LoginActivity.java
+++ app/src/main/java/org/unicef/rapidreg/login/LoginActivity.java
@@ -29,7 +29,6 @@ public class LoginActivity extends MvpActivity<LoginView, LoginPresenter> implem
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         loginProgressDialog = new ProgressDialog(this);
