59c0d550f6d6cbdbe20fcfd353cf1eb7d1adc7aa
shane51 <suse51@163.com>
2016-05-23 14:34:01 +0800

    fix merge

--- app/src/main/java/org/unicef/rapidreg/login/LoginView.java
+++ app/src/main/java/org/unicef/rapidreg/login/LoginView.java
@@ -1,5 +1,7 @@
 package org.unicef.rapidreg.login;
 
+import android.widget.EditText;
+
 import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
 
 import org.unicef.rapidreg.model.LoginResponse;
@@ -10,4 +12,7 @@ public interface LoginView extends MvpLceView<Call<LoginResponse>> {
     public void showLoginResult(String message);
     public void showProgressDialog();
     public void dismissProgressDialog();
+    public EditText getUsernameView();
+    public EditText getPasswordView();
+    public EditText getUrlView();
 }
