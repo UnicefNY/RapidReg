670829afd9dc7e72866c1469e78eb17bc56271a6
bfeng <bfeng@thoughtworks.com>
2016-05-30 14:46:02 +0800

    1. add interface to get application's context in PrimeroApplication class 2. add config and method to get if it's debug mode 3. generate encryption key for sqlcipher in release mode

--- app/src/main/java/org/unicef/rapidreg/db/SQLCipherHelperImpl.java
+++ app/src/main/java/org/unicef/rapidreg/db/SQLCipherHelperImpl.java
@@ -1,10 +1,21 @@
 package org.unicef.rapidreg.db;
 
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.BuildConfig;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
 import com.raizlabs.android.dbflow.config.DatabaseDefinition;
 import com.raizlabs.android.dbflow.structure.database.DatabaseHelperListener;
 import com.raizlabs.dbflow.android.sqlcipher.SQLCipherOpenHelper;
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.unicef.rapidreg.PrimeroApplication;
+
 public class SQLCipherHelperImpl extends SQLCipherOpenHelper {
+    public static final String TAG = SQLCipherHelperImpl.class.getSimpleName();
 
     public SQLCipherHelperImpl(DatabaseDefinition definition, DatabaseHelperListener listener) {
         super(definition, listener);
@@ -12,6 +23,22 @@ public class SQLCipherHelperImpl extends SQLCipherOpenHelper {
 
     @Override
     protected String getCipherSecret() {
-        return "primero-db-rules";
+        Log.d(TAG, String.valueOf(BuildConfig.DEBUG));
+        return PrimeroApplication.isDebugMode() ? "primero" : generateCipherKey();
+    }
+
+    private String generateCipherKey() {
+        Context ctx = PrimeroApplication.getAppContext();
+        String android_id = Settings.Secure.getString(ctx.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        String device_id = tm.getDeviceId();
+
+        String build_info = Build.BOARD + Build.BRAND + Build.DEVICE + Build.DISPLAY
+                + Build.FINGERPRINT + Build.HOST + Build.ID + Build.MANUFACTURER + Build.MODEL
+                + Build.PRODUCT + Build.TAGS + Build.TYPE + Build.USER;
+
+        return BCrypt.hashpw(build_info + device_id + android_id, BCrypt.gensalt(12));
     }
 }
