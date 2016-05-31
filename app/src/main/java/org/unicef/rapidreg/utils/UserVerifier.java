d130270f7ce82af6a69e2e569ad2eedb00efe171
bfeng <bfeng@thoughtworks.com>
2016-05-31 15:08:23 +0800

    1. mark username as unique 2. refactor user verifying logic

---
+++ app/src/main/java/org/unicef/rapidreg/utils/UserVerifier.java
@@ -0,0 +1,43 @@
+package org.unicef.rapidreg.utils;
+
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import org.unicef.rapidreg.R;
+import org.unicef.rapidreg.model.User;
+import org.unicef.rapidreg.model.User_Table;
+import org.unicef.rapidreg.utils.EncryptHelper;
+
+public class UserVerifier {
+    public enum VerifiedCode {
+        USER_DOES_NOT_EXIST(R.string.login_offline_no_user_text),
+        PASSWORD_INCORRECT(R.string.login_failed_text),
+        OK(R.string.login_offline_success_text);
+
+        private int resId;
+
+        VerifiedCode(int resId) {
+            this.resId = resId;
+        }
+
+        public int getResId() {
+            return resId;
+        }
+    }
+
+    public static VerifiedCode verify(String username, String password) {
+        User user = SQLite.select()
+                .from(User.class)
+                .where(User_Table.user_name.eq(username))
+                .querySingle();
+
+        if (user == null) {
+            return VerifiedCode.USER_DOES_NOT_EXIST;
+        }
+
+        if (EncryptHelper.isMatched(password, user.getPassword())) {
+            return VerifiedCode.OK;
+        } else {
+            return VerifiedCode.PASSWORD_INCORRECT;
+        }
+    }
+}
