eb4552c1ff95929a2f0286a8a9d2d4d8f8370ed7
bfeng <bfeng@thoughtworks.com>
2016-06-01 14:51:28 +0800

    format code

--- app/src/main/java/org/unicef/rapidreg/db/helper/UserDBHelper.java
+++ app/src/main/java/org/unicef/rapidreg/db/helper/UserDBHelper.java
@@ -10,7 +10,8 @@ import java.util.List;
 public class UserDBHelper {
 
     public static User getUserByName(String username) {
-        return SQLite.select().from(User.class).where(User_Table.user_name.eq(username)).querySingle();
+        return SQLite.select().from(User.class)
+                .where(User_Table.user_name.eq(username)).querySingle();
     }
 
     public static List<User> getAllUsers() {
