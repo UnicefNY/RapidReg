08fb908f43ba9613a11c30ce25cad1c83c254954
bfeng <bfeng@thoughtworks.com>
2016-06-01 14:47:57 +0800

    refactor: change method's name

--- app/src/main/java/org/unicef/rapidreg/db/helper/UserDBHelper.java
+++ app/src/main/java/org/unicef/rapidreg/db/helper/UserDBHelper.java
@@ -5,11 +5,15 @@ import com.raizlabs.android.dbflow.sql.language.SQLite;
 import org.unicef.rapidreg.model.User;
 import org.unicef.rapidreg.model.User_Table;
 
+import java.util.List;
+
 public class UserDBHelper {
-    public static User getUser(String username) {
-        return SQLite.select()
-                .from(User.class)
-                .where(User_Table.user_name.eq(username))
-                .querySingle();
+
+    public static User getUserByName(String username) {
+        return SQLite.select().from(User.class).where(User_Table.user_name.eq(username)).querySingle();
+    }
+
+    public static List<User> getAllUsers() {
+        return SQLite.select().from(User.class).queryList();
     }
 }
