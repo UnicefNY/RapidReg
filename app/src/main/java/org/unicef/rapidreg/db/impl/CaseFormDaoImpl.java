4bfa6d7b6695fd624a5ed07d05ef8a0c13aa5075
bfeng <bfeng@thoughtworks.com>
2016-06-06 15:57:34 +0800

    add the method in CaseFormService to save or update case form

--- app/src/main/java/org/unicef/rapidreg/db/impl/CaseFormDaoImpl.java
+++ app/src/main/java/org/unicef/rapidreg/db/impl/CaseFormDaoImpl.java
@@ -4,14 +4,19 @@ import com.raizlabs.android.dbflow.data.Blob;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import org.unicef.rapidreg.db.CaseFormDao;
-import org.unicef.rapidreg.model.ChildCase;
+import org.unicef.rapidreg.model.CaseForm;
 
 public class CaseFormDaoImpl implements CaseFormDao {
 
     @Override
     public Blob getForm() {
-        ChildCase childCase = SQLite.select().from(ChildCase.class).querySingle();
+        CaseForm caseForm = SQLite.select().from(CaseForm.class).querySingle();
 
-        return childCase == null ? null : childCase.getForm();
+        return caseForm == null ? null : caseForm.getForm();
+    }
+
+    @Override
+    public CaseForm getCaseForm() {
+        return SQLite.select().from(CaseForm.class).querySingle();
     }
 }
--- app/src/main/java/org/unicef/rapidreg/service/CaseFormService.java
+++ app/src/main/java/org/unicef/rapidreg/service/CaseFormService.java
@@ -1,15 +1,18 @@
 package org.unicef.rapidreg.service;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.raizlabs.android.dbflow.data.Blob;
 
 import org.unicef.rapidreg.db.CaseFormDao;
 import org.unicef.rapidreg.db.impl.CaseFormDaoImpl;
+import org.unicef.rapidreg.model.CaseForm;
 import org.unicef.rapidreg.model.form.childcase.CaseFormRoot;
 
 public class CaseFormService {
+    public static final String TAG = CaseFormService.class.getSimpleName();
     private static final CaseFormService CASE_FORM_SERVICE
             = new CaseFormService(new CaseFormDaoImpl());
     private CaseFormDao caseFormDao;
@@ -27,5 +30,18 @@ public class CaseFormService {
         String formJson = new String(form.getBlob());
         return TextUtils.isEmpty(formJson) ?
                 null : new Gson().fromJson(formJson, CaseFormRoot.class);
+    }
+
+    public void saveOrUpdateCaseForm(CaseForm caseForm) {
+        CaseForm existingCaseForm = caseFormDao.getCaseForm();
+
+        if (existingCaseForm == null) {
+            Log.d(TAG, "save new case form");
+            caseForm.save();
+        } else {
+            Log.d(TAG, "update existing case form");
+            existingCaseForm.setForm(caseForm.getForm());
+            existingCaseForm.update();
+        }
     }
 }
