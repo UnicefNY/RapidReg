96d614152a8df489cf96cec812a1b2ea36095c85
bfeng <bfeng@thoughtworks.com>
2016-06-07 18:19:31 +0800

    format code

--- app/src/main/java/org/unicef/rapidreg/childcase/CasesRegisterView.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CasesRegisterView.java
@@ -2,8 +2,6 @@ package org.unicef.rapidreg.childcase;
 
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
-import org.unicef.rapidreg.forms.childcase.CaseFormRoot;
-
 public interface CasesRegisterView extends MvpView {
 
     void initView(CasesRegisterAdapter adapter);
