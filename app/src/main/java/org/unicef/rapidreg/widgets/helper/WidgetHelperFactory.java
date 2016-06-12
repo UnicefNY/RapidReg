12affffad4e39ce7359a5dcb054fa940b236fea7
bfeng <bfeng@thoughtworks.com>
2016-06-12 17:03:55 +0800

    refactor: WidgetHelperFactory

--- app/src/main/java/org/unicef/rapidreg/widgets/helper/WidgetHelperFactory.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/helper/WidgetHelperFactory.java
@@ -8,12 +8,15 @@ public class WidgetHelperFactory {
 
     public static WidgetHelper getWidgetHelper(Context context, CaseField field) {
         String type = field.getType();
+
         if (CaseField.TYPE_SEPARATOR.equalsIgnoreCase(type)) {
             return new SeparatorHelper(context, field);
-        } else if (CaseField.TYPE_TICK_BOX.equalsIgnoreCase(type)) {
-            return new TickBoxHelper(context, field);
-        } else {
-            return new GenericHelper(context, field);
         }
+
+        if (CaseField.TYPE_TICK_BOX.equalsIgnoreCase(type)) {
+            return new TickBoxHelper(context, field);
+        }
+
+        return new GenericHelper(context, field);
     }
 }
