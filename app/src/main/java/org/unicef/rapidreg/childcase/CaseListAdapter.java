1ded5052ad5100230063bf50551f7f879d0bc0a0
CHEN Yang <yangchen@thoughtworks.com>
2016-06-14 15:03:09 +0800

    feature/#38--implement show details when click any case

--- app/src/main/java/org/unicef/rapidreg/childcase/CaseListAdapter.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CaseListAdapter.java
@@ -1,5 +1,6 @@
 package org.unicef.rapidreg.childcase;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import org.unicef.rapidreg.R;
+import org.unicef.rapidreg.base.view.BaseActivity;
 import org.unicef.rapidreg.model.Case;
 import org.unicef.rapidreg.service.CaseService;
 import java.lang.reflect.Type;
@@ -23,9 +25,15 @@ import java.util.Map;
 public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseListHolder>{
 
     private List<Case> caseList;
+    private CaseService caseService;
+    private Context context;
     private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
     public CaseListAdapter() {
+    }
+
+    public CaseListAdapter(Context context) {
+        this.context = context;
         caseList = new ArrayList<>();
         caseList = getAllCaseData();
     }
@@ -41,12 +49,20 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
         Case caseItem = caseList.get(position);
         String caseJson = new String(caseItem.getContent().getBlob());
         Type type = new TypeToken<Map<String, String>>() {}.getType();
-        Map<String, String> caseInfo = new Gson().fromJson(caseJson, type);
+        final Map<String, String> caseInfo = new Gson().fromJson(caseJson, type);
         holder.caseTitle.setText(caseItem.getUniqueId().substring(0, 7));
         holder.caseChildGender.setText(caseInfo.get("Sex"));
         holder.caseChildAge.setText(caseInfo.get("Age"));
         holder.caseCreateTime.setText(formatter.format(caseItem.getCreateAt()));
-
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CaseService.CaseValues.setValues(caseInfo);
+                ((BaseActivity) context).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_content, new CaseRegisterWrapperFragment())
+                        .commit();
+            }
+        });
     }
 
     @Override
@@ -55,7 +71,7 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
     }
 
     private List<Case> getAllCaseData() {
-        CaseService caseService = CaseService.getInstance();
+        caseService = CaseService.getInstance();
         return caseService.getCaseList();
     }
 
@@ -65,9 +81,11 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
         protected TextView caseChildAge;
         protected TextView caseCreateTime;
         protected ImageView caseImage;
+        protected View view;
 
         public CaseListHolder(View itemView) {
             super(itemView);
+            view = itemView;
             caseTitle = (TextView) itemView.findViewById(R.id.case_title);
             caseChildGender = (TextView) itemView.findViewById(R.id.case_child_gender);
             caseChildAge = (TextView) itemView.findViewById(R.id.case_child_age);
