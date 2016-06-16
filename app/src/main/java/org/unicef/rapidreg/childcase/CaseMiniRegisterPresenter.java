61203d90b065fe8e7cf07c3850180b0c0364e938
CHEN Yang <yangchen@thoughtworks.com>
2016-06-16 16:10:08 +0800

    feature/#40--can show mini case form

--- app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterPresenter.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterPresenter.java
@@ -1,6 +1,33 @@
 package org.unicef.rapidreg.childcase;
 
+import android.content.Context;
+
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 
+import org.unicef.rapidreg.forms.childcase.CaseField;
+import org.unicef.rapidreg.forms.childcase.CaseFormRoot;
+import org.unicef.rapidreg.forms.childcase.CaseSection;
+import org.unicef.rapidreg.service.CaseFormService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaseMiniRegisterPresenter extends MvpBasePresenter<CaseMiniRegisterView> {
+
+    public void initContext(Context context) {
+        if (isViewAttached()) {
+            CaseFormRoot form = CaseFormService.getInstance().getCurrentForm();
+            if (form != null) {
+                List<CaseField> fields = new ArrayList<>();
+                for (CaseSection section : form.getSections()) {
+                    for (CaseField caseField : section.getFields()) {
+                        if (caseField.isShowOnMiniForm()) {
+                            fields.add(caseField);
+                        }
+                    }
+                }
+                getView().initView(new CaseMiniRegisterAdapter(context, -1, fields));
+            }
+        }
+    }
 }
--- app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterView.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterView.java
@@ -3,4 +3,7 @@ package org.unicef.rapidreg.childcase;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 public interface CaseMiniRegisterView extends MvpView {
+
+    void initView(CaseMiniRegisterAdapter adapter);
+
 }
