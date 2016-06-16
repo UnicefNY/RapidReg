e63874b6471e230d46f0b64e11bb2f85125fc51a
shane51 <suse51@163.com>
2016-06-16 21:26:54 +0800

    feature/forms -- required field reminder

--- app/src/main/java/org/unicef/rapidreg/widgets/helper/BaseWidgetHelper.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/helper/BaseWidgetHelper.java
@@ -35,6 +35,10 @@ public abstract class BaseWidgetHelper implements WidgetHelper {
        return field.isEditable();
     }
 
+    protected boolean isRequired() {
+        return field.isRequired();
+    }
+
     private void initRes() {
         inflater = LayoutInflater.from(context);
         resources = context.getResources();
