12acb4d41dab2630b5978f7a4b580d42a198ad6e
CHEN Yang <yangchen@thoughtworks.com>
2016-06-17 17:30:30 +0800

    feature/#38--when no mini form should show full form

--- app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterAdapter.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterAdapter.java
@@ -1,7 +1,6 @@
 package org.unicef.rapidreg.childcase;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
