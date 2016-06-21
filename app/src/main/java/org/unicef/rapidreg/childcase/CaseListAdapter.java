a2f7808562b29038ceeddd41411f7d801f9020a5
bfeng <bfeng@thoughtworks.com>
2016-06-21 14:49:18 +0800

    [#35][UI] - change case list UI as per new design

--- app/src/main/java/org/unicef/rapidreg/childcase/CaseListAdapter.java
+++ app/src/main/java/org/unicef/rapidreg/childcase/CaseListAdapter.java
@@ -2,8 +2,15 @@ package org.unicef.rapidreg.childcase;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,19 +25,19 @@ import org.unicef.rapidreg.model.Case;
 import org.unicef.rapidreg.service.CaseService;
 
 import java.lang.reflect.Type;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseListHolder> {
-
     public static final String TAG = CaseListAdapter.class.getSimpleName();
 
     private List<Case> caseList;
-    private CaseService caseService;
     private Context context;
-    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    private DateFormat dateFormat = SimpleDateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US);
     private boolean isDetailShow = true;
 
     public CaseListAdapter(Context context) {
@@ -45,7 +52,9 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
 
     @Override
     public CaseListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.case_list_layout, parent, false);
+        ViewGroup itemView = (ViewGroup) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.case_list, parent, false);
+
         return new CaseListHolder(itemView);
     }
 
@@ -60,18 +69,22 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
 
         final Map<String, String> caseInfo = new Gson().fromJson(caseJson, type);
 
-        caseInfo.put(CaseService.UNIQUE_ID,caseItem.getUniqueId());
+        caseInfo.put(CaseService.UNIQUE_ID, caseItem.getUniqueId());
+        Gender gender = Gender.valueOf(caseInfo.get("Sex").toUpperCase());
 
-        holder.caseTitle.setText(caseItem.getUniqueId().substring(0, 7));
-        holder.caseChildGender.setText(caseInfo.get("Sex"));
-        holder.caseChildAge.setText(caseInfo.get("Age"));
-        holder.caseCreateTime.setText(formatter.format(caseItem.getCreateAt()));
+        holder.caseImage.setImageDrawable(getDefaultAvatar(gender.getAvatarId()));
+        String shortUUID = getShortUUID(caseItem.getUniqueId());
+        holder.id_normal_state.setText(shortUUID);
+        holder.id_hidden_state.setText(shortUUID);
+        holder.genderBadge.setImageDrawable(getDefaultGenderBadge(gender.getGenderId()));
+        holder.genderName.setText(gender.getName());
+        holder.genderName.setTextColor(ContextCompat.getColor(context, gender.getColorId()));
+        holder.age.setText(caseInfo.get("Age"));
+        holder.createdTime.setText(dateFormat.format(caseItem.getCreateAt()));
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CaseService.CaseValues.setValues(caseInfo);
-                Log.i("sjyuan", caseInfo.toString());
-
                 CaseActivity caseActivity = (CaseActivity) context;
                 setViewMode(caseActivity);
 
@@ -89,14 +102,14 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
             }
         });
 
-        hideDetailIfNeeded(holder);
+        toggleTextArea(holder);
     }
-
 
     @Override
     public int getItemCount() {
         return caseList.size();
     }
+
 
     public boolean isListEmpty() {
         return getItemCount() == 0;
@@ -107,36 +120,97 @@ public class CaseListAdapter extends RecyclerView.Adapter<CaseListAdapter.CaseLi
         this.notifyDataSetChanged();
     }
 
-    private void hideDetailIfNeeded(CaseListHolder holder) {
-        if (!isDetailShow) {
-            holder.caseChildGender.setText("*****");
-            holder.caseChildAge.setText("***");
-            holder.caseCreateTime.setText("******");
-        }
+    private void toggleTextArea(CaseListHolder holder) {
+        holder.textAreaNormalState.setVisibility(isDetailShow ? View.VISIBLE : View.GONE);
+        holder.textAreaHiddenState.setVisibility(isDetailShow ? View.GONE : View.VISIBLE);
     }
 
     private List<Case> getAllCaseData() {
-        caseService = CaseService.getInstance();
+        CaseService caseService = CaseService.getInstance();
         return caseService.getCaseList();
     }
 
+    private Drawable getDefaultGenderBadge(int genderId) {
+        return ResourcesCompat.getDrawable(context.getResources(), genderId, null);
+    }
+
+    private Drawable getDefaultAvatar(int resId) {
+        return getRoundedDrawable(resId);
+    }
+
+    private Drawable getRoundedDrawable(int resId) {
+        Resources resources = this.context.getResources();
+        Bitmap img = BitmapFactory.decodeResource(resources, resId);
+        RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(resources, img);
+        dr.setCornerRadius(Math.max(img.getWidth(), img.getHeight()) / 2.0f);
+
+        return dr;
+    }
+
+    private String getShortUUID(String uuid) {
+        int length = uuid.length();
+        return length > 7 ? uuid.substring(length - 7) : uuid;
+    }
+
     public static class CaseListHolder extends RecyclerView.ViewHolder {
-        protected TextView caseTitle;
-        protected TextView caseChildGender;
-        protected TextView caseChildAge;
-        protected TextView caseCreateTime;
+        protected TextView id_normal_state;
+        protected TextView id_hidden_state;
+        protected ImageView genderBadge;
+        protected TextView genderName;
+        protected TextView age;
+        protected TextView createdTime;
         protected ImageView caseImage;
         protected View view;
+        protected View textAreaNormalState;
+        protected View textAreaHiddenState;
 
         public CaseListHolder(View itemView) {
             super(itemView);
             view = itemView;
 
-            caseTitle = (TextView) itemView.findViewById(R.id.case_title);
-            caseChildGender = (TextView) itemView.findViewById(R.id.case_child_gender);
-            caseChildAge = (TextView) itemView.findViewById(R.id.case_child_age);
-            caseCreateTime = (TextView) itemView.findViewById(R.id.case_create_time);
+            id_normal_state = (TextView) itemView.findViewById(R.id.id_normal_state);
+            id_hidden_state = (TextView) itemView.findViewById(R.id.id_on_hidden_state);
+            genderBadge = (ImageView) itemView.findViewById(R.id.gender_badge);
+            genderName = (TextView) itemView.findViewById(R.id.gender_name);
+            age = (TextView) itemView.findViewById(R.id.age);
+            createdTime = (TextView) itemView.findViewById(R.id.created_time);
             caseImage = (ImageView) itemView.findViewById(R.id.case_image);
+
+            textAreaNormalState = itemView.findViewById(R.id.text_area_normal_state);
+            textAreaHiddenState = itemView.findViewById(R.id.text_area_hidden_state);
+        }
+    }
+
+    public enum Gender {
+        MALE("BOY", R.drawable.avatar_boy, R.drawable.boy, R.color.boy_blue),
+        FEMALE("GIRL", R.drawable.avatar_girl, R.drawable.girl, R.color.girl_red);
+
+        private String name;
+        private int avatarId;
+        private int genderId;
+        private int colorId;
+
+        Gender(String name, int avatarId, int genderId, int colorId) {
+            this.name = name;
+            this.avatarId = avatarId;
+            this.genderId = genderId;
+            this.colorId = colorId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAvatarId() {
+            return avatarId;
+        }
+
+        public int getGenderId() {
+            return genderId;
+        }
+
+        public int getColorId() {
+            return colorId;
         }
     }
 }
