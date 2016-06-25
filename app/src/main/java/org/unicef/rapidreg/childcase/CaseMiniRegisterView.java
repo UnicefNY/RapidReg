72e055f7dfb887e29770e29402d5928546ed6b0f
CHEN Yang <yangchen@thoughtworks.com>
2016-06-25 12:24:39 +0800

    refactor/remove unused class

--- app/src/main/java/org/unicef/rapidreg/childcase/CaseMiniRegisterView.java
+++
@@ -1,9 +0,0 @@
-package org.unicef.rapidreg.childcase;
-
-import com.hannesdorfmann.mosby.mvp.MvpView;
-
-public interface CaseMiniRegisterView extends MvpView {
-
-    void initView(CaseMiniRegisterAdapter adapter);
-
-}
