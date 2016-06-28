8281a8f2737787eb842362049d7b9e31b67829de
shane51 <suse51@163.com>
2016-06-28 09:54:33 +0800

    feature/cases --- new audio recorder activity

---
+++ app/src/main/java/org/unicef/rapidreg/childcase/AudioRecorderActivity.java
@@ -0,0 +1,189 @@
+package org.unicef.rapidreg.childcase;
+
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.unicef.rapidreg.R;
+import org.unicef.rapidreg.service.cache.CaseFieldValueCache;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AudioRecorderActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "AudioRecordTest";
+    public static final int RECORDER_MAX_DURATION_MS = 10000;
+    private static String mFileName = null;
+
+    @BindView(R.id.stop_button)
+    ImageView stopButton;
+    @BindView(R.id.audio_timer)
+    TextView timerTextView;
+    @BindView(R.id.audio_timer_total)
+    TextView timerTotalTextView;
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    private boolean mStartRecording = true;
+    private boolean mStartPlaying = true;
+
+    private long startTime = 0;
+    private int audioDuration = 0;
+
+
+    final Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+
+            timerTextView.setText(getTime(millis));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    private String getTime(long millis) {
+        int seconds = (int) (millis / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        return String.format("%d:%02d", minutes, seconds);
+    }
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_record_layout);
+        ButterKnife.bind(this);
+        mFileName = CaseFieldValueCache.AUDIO_FILE_PATH;
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("currentState");
+            if (TextUtils.equals(value,  "StartRecording")) {
+                recordAudio();
+            } else if (TextUtils.equals(value,  "StartPlaying")) {
+                playAudio();
+            }
+        }
+    }
+
+    @OnClick(R.id.stop_button)
+    public void onStopButtonClicked() {
+        stopRecording();
+        stopPlaying();
+        finish();
+    }
+
+    public void recordAudio() {
+        onRecord(mStartRecording);
+        mStartRecording = !mStartRecording;
+    }
+
+
+    public void playAudio() {
+        onPlay(mStartPlaying);
+        mStartPlaying = !mStartPlaying;
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+            startTiming();
+        } else {
+            stopTiming();
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+            startTiming();
+        } else {
+            stopTiming();
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    onStopButtonClicked();
+                }
+            });
+            mPlayer.prepare();
+            audioDuration = mPlayer.getDuration();
+            timerTotalTextView.setText("/" + getTime(audioDuration));
+            mPlayer.start();
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mRecorder.setMaxDuration(RECORDER_MAX_DURATION_MS);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+        mRecorder.start();
+        mRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+            @Override
+            public void onInfo(MediaRecorder mr, int what, int extra) {
+                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
+                    onStopButtonClicked();
+                }
+            }
+        });
+    }
+
+    private void stopRecording() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+
+    public void startTiming() {
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    public void stopTiming() {
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+}
--- app/src/main/java/org/unicef/rapidreg/widgets/viewholder/AudioUploadViewHolder.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/viewholder/AudioUploadViewHolder.java
@@ -1,20 +1,17 @@
 package org.unicef.rapidreg.widgets.viewholder;
 
 import android.content.Context;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.os.Handler;
-import android.util.Log;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.unicef.rapidreg.R;
+import org.unicef.rapidreg.childcase.AudioRecorderActivity;
 import org.unicef.rapidreg.childcase.CaseActivity;
 import org.unicef.rapidreg.service.cache.CaseFieldValueCache;
 
 import java.io.File;
-import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,40 +31,9 @@ public class AudioUploadViewHolder extends BaseViewHolder {
     ImageView playButton;
     @BindView(R.id.delete_button)
     ImageView audioDeleteButton;
-    @BindView(R.id.audio_timer)
-    TextView timerTextView;
-    @BindView(R.id.audio_timer_total)
-    TextView timerTotalTextView;
+    @BindView(R.id.no_file_text_view)
+    TextView noFileTextView;
     private CaseActivity caseActivity;
-    private MediaRecorder mRecorder = null;
-    private MediaPlayer mPlayer = null;
-
-    private boolean mStartRecording = true;
-    private boolean mStartPlaying = true;
-
-    private long startTime = 0;
-    private int audioDuration = 0;
-
-
-    final Handler timerHandler = new Handler();
-    Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            long millis = System.currentTimeMillis() - startTime;
-
-            timerTextView.setText(getTime(millis));
-
-            timerHandler.postDelayed(this, 500);
-        }
-    };
-
-    private String getTime(long millis) {
-        int seconds = (int) (millis / 1000);
-        int minutes = seconds / 60;
-        seconds = seconds % 60;
-
-        return String.format("%d:%02d", minutes, seconds);
-    }
 
 
     public AudioUploadViewHolder(Context context, View itemView) {
@@ -92,7 +58,7 @@ public class AudioUploadViewHolder extends BaseViewHolder {
 
     public boolean isCaseMode(CaseActivity.CaseMode mode) {
         CaseActivity.CaseMode caseMode = (CaseActivity.CaseMode) caseActivity.getIntent().getExtras().get(CaseActivity.INTENT_KEY_CASE_MODE);
-        if (caseMode ==  mode) {
+        if (caseMode == mode) {
             return true;
         }
         return false;
@@ -101,14 +67,13 @@ public class AudioUploadViewHolder extends BaseViewHolder {
 
     @OnClick(R.id.record_button)
     public void onRecordButtonClicked() {
-        onRecord(mStartRecording);
-        if (mStartRecording) {
-            recordButton.setImageResource(R.drawable.pause_210_with_color_boarder);
-        } else {
-            initPlayAudioUI();
-            showDeleteIconWhenIsEditMode();
-        }
-        mStartRecording = !mStartRecording;
+        Intent intent = new Intent(caseActivity, AudioRecorderActivity.class);
+        intent.putExtra("currentState", "StartRecording");
+        int requestCode = 1;
+        caseActivity.startActivityForResult(intent, requestCode);
+        recordButton.setVisibility(View.GONE);
+        playButton.setVisibility(View.VISIBLE);
+        showDeleteIconWhenIsEditMode();
     }
 
     private void showDeleteIconWhenIsEditMode() {
@@ -124,22 +89,17 @@ public class AudioUploadViewHolder extends BaseViewHolder {
         } else {
             recordButton.setVisibility(View.GONE);
             playButton.setVisibility(View.GONE);
-            timerTextView.setVisibility(View.GONE);
-            timerTotalTextView.setText("No audio file exist.");
+            noFileTextView.setText("No audio file exist.");
         }
     }
 
+
     @OnClick(R.id.play_button)
     public void onPlayButtonClicked() {
-        onPlay(mStartPlaying);
-        if (mStartPlaying) {
-            playButton.setImageResource(R.drawable.pause_210_with_color_boarder);
-            audioDeleteButton.setVisibility(View.GONE);
-        } else {
-            playButton.setImageResource(R.drawable.play_210);
-            showDeleteIconWhenIsEditMode();
-        }
-        mStartPlaying = !mStartPlaying;
+        Intent intent = new Intent(caseActivity, AudioRecorderActivity.class);
+        intent.putExtra("currentState", "StartPlaying");
+        int requestCode = 2;
+        caseActivity.startActivityForResult(intent, requestCode);
     }
 
     private boolean isFileExists(String filePath) {
@@ -162,97 +122,5 @@ public class AudioUploadViewHolder extends BaseViewHolder {
         recordButton.setImageResource(R.drawable.mic_rec_210);
         playButton.setVisibility(View.GONE);
         audioDeleteButton.setVisibility(View.GONE);
-        timerTextView.setText("0:00");
-        timerTotalTextView.setText("/1:00");
     }
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-            startTiming();
-        } else {
-            stopTiming();
-            stopRecording();
-        }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-            startTiming();
-        } else {
-            stopTiming();
-            stopPlaying();
-        }
-    }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    if (mPlayer != null) {
-                        onPlayButtonClicked();
-                    }
-                }
-            });
-            mPlayer.prepare();
-            audioDuration = mPlayer.getDuration();
-            timerTotalTextView.setText("/" + getTime(audioDuration));
-            mPlayer.start();
-
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mRecorder.setMaxDuration(RECORDER_MAX_DURATION_MS);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-        mRecorder.start();
-        mRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-            @Override
-            public void onInfo(MediaRecorder mr, int what, int extra) {
-                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
-                    onRecordButtonClicked();
-                }
-            }
-        });
-    }
-
-    private void stopRecording() {
-        if (mRecorder != null) {
-            mRecorder.stop();
-            mRecorder.release();
-            mRecorder = null;
-        }
-    }
-
-
-    public void startTiming() {
-        startTime = System.currentTimeMillis();
-        timerHandler.postDelayed(timerRunnable, 0);
-    }
-
-    public void stopTiming() {
-        timerHandler.removeCallbacks(timerRunnable);
-    }
-
 }
