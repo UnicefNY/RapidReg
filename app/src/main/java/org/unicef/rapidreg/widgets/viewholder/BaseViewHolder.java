3f5b0a619c09d01e18c8724c254948f6a09a3174
bfeng <bfeng@thoughtworks.com>
2016-06-28 17:48:04 +0800

    [#47] - restore subform values after scrolling tabs

--- app/src/main/java/org/unicef/rapidreg/widgets/viewholder/BaseViewHolder.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/viewholder/BaseViewHolder.java
@@ -1,11 +1,17 @@
 package org.unicef.rapidreg.widgets.viewholder;
 
 import android.content.Context;
+import android.support.v4.util.ArrayMap;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import org.unicef.rapidreg.R;
 import org.unicef.rapidreg.forms.childcase.CaseField;
+import org.unicef.rapidreg.service.cache.SubformCache;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
 
@@ -18,9 +24,11 @@ public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
         this.context = context;
     }
 
-    public void setValue(T field){}
+    public void setValue(T field) {
+    }
 
-    public void setOnClickListener(T field){}
+    public void setOnClickListener(T field) {
+    }
 
     //TODO: need to get display name according to the current system language
     protected String getLabel(CaseField field) {
@@ -43,5 +51,23 @@ public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
             itemView.setBackgroundResource(R.color.white);
             itemView.setEnabled(true);
         }
+    }
+
+    protected boolean isSubformField(CaseField field) {
+        return field.getParent() != null;
+    }
+
+    protected String getValue(CaseField field) {
+        List<Map<String, String>> values = SubformCache.get(field.getParent()) == null ?
+                new ArrayList<Map<String, String>>() : SubformCache.get(field.getParent());
+        Map<String, String> value;
+
+        try {
+            value = values.get(field.getIndex());
+        } catch (IndexOutOfBoundsException e) {
+            value = new ArrayMap<>();
+        }
+
+        return value.get(getLabel(field));
     }
 }
--- app/src/main/java/org/unicef/rapidreg/widgets/viewholder/TickBoxViewHolder.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/viewholder/TickBoxViewHolder.java
@@ -8,6 +8,12 @@ import android.widget.TextView;
 import org.unicef.rapidreg.R;
 import org.unicef.rapidreg.forms.childcase.CaseField;
 import org.unicef.rapidreg.service.cache.CaseFieldValueCache;
+import org.unicef.rapidreg.service.cache.SubformCache;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,7 +35,12 @@ public class TickBoxViewHolder extends BaseViewHolder<CaseField> {
     public void setValue(CaseField field) {
         labelView.setText(getLabel(field));
         disableUnediatbleField(field);
-        valueView.setChecked(Boolean.valueOf(CaseFieldValueCache.get(getLabel(field))));
+
+        if (isSubformField(field)) {
+            valueView.setChecked(Boolean.valueOf(getValue(field)));
+        } else {
+            valueView.setChecked(Boolean.valueOf(CaseFieldValueCache.get(getLabel(field))));
+        }
     }
 
     @Override
@@ -37,9 +48,33 @@ public class TickBoxViewHolder extends BaseViewHolder<CaseField> {
         valueView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CaseFieldValueCache.put(getLabel(field),
-                        String.valueOf(((CheckBox) v).isChecked()));
+
+                if (isSubformField(field)) {
+                    SubformCache.put(field.getParent(), getValues(field,
+                            ((CheckBox) v).isChecked()));
+                } else {
+                    CaseFieldValueCache.put(getLabel(field),
+                            String.valueOf(((CheckBox) v).isChecked()));
+                }
             }
         });
+    }
+
+    private List<Map<String, String>> getValues(CaseField field, boolean isChecked) {
+        List<Map<String, String>> values = SubformCache.get(field.getParent()) == null ?
+                new ArrayList<Map<String, String>>() : SubformCache.get(field.getParent());
+
+        Map<String, String> value;
+        try {
+            value = values.get(field.getIndex());
+            value.put(field.getDisplayName().get("en"), String.valueOf(isChecked));
+            values.set(field.getIndex(), value);
+        } catch (IndexOutOfBoundsException e) {
+            value = new HashMap<>();
+            value.put(field.getDisplayName().get("en"), String.valueOf(isChecked));
+            values.add(field.getIndex(), value);
+        }
+
+        return values;
     }
 }
