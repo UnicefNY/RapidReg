784d94be2c43a25471f400da927d6a347506da39
bfeng <bfeng@thoughtworks.com>
2016-06-28 09:48:27 +0800

    [#47] - optimize the nested form adding and removing dynamically

--- app/src/main/java/org/unicef/rapidreg/widgets/viewholder/SubformViewHolder.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/viewholder/SubformViewHolder.java
@@ -20,31 +20,27 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SubformViewHolder extends BaseViewHolder<CaseField> {
+    public static final int NUM_CHILD_VIEWS = 2;
 
     @BindView(R.id.show_subform)
     Button showSubform;
 
     private CaseActivity activity;
     private ViewGroup parent;
-    private LinearLayout container;
     private List<CaseField> fields;
-    private RecyclerView fieldList;
-    private Button addSubForm;
-    private Button deleteSubForm;
 
     public SubformViewHolder(Context context, View itemView) {
         super(context, itemView);
         ButterKnife.bind(this, itemView);
         activity = (CaseActivity) context;
         parent = (ViewGroup) itemView;
-
-        initView();
     }
 
     @Override
     public void setValue(CaseField field) {
         fields = field.getSubForm().getFields();
-        showSubform.setText(String.format("%s %s", showSubform.getText(), field.getDisplayName().get("en")));
+        showSubform.setText(String.format("%s %s", showSubform.getText(),
+                field.getDisplayName().get("en")));
     }
 
     @Override
@@ -52,53 +48,37 @@ public class SubformViewHolder extends BaseViewHolder<CaseField> {
         showSubform.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CaseRegisterAdapter adapter = new CaseRegisterAdapter(activity, fields);
-
-                RecyclerView.LayoutManager layout = new LinearLayoutManager(activity);
-                layout.setAutoMeasureEnabled(true);
-                fieldList.setLayoutManager(layout);
-                fieldList.setAdapter(adapter);
-                showSubform.setVisibility(View.GONE);
-                parent.addView(container);
-            }
-        });
-
-        addSubForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 LayoutInflater inflater = LayoutInflater.from(activity);
                 ViewGroup container = (LinearLayout) inflater
                         .inflate(R.layout.form_subform_layout, parent, false);
-                RecyclerView fieldList = (RecyclerView) container.findViewById(R.id.field_list);
 
-                CaseRegisterAdapter adapter = new CaseRegisterAdapter(activity, fields);
-                RecyclerView.LayoutManager layout = new LinearLayoutManager(activity);
-                layout.setAutoMeasureEnabled(true);
-                fieldList.setLayoutManager(layout);
-                fieldList.setAdapter(adapter);
-                parent.addView(container);
-            }
-        });
-
-        deleteSubForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parent.removeView((View) v.getParent());
-
-                if (parent.getChildCount() == 1) {
-                    showSubform.setVisibility(View.VISIBLE);
-                }
+                initDeleteBtn(container);
+                initFieldList(container);
+                parent.addView(container, getInsertIndex());
             }
         });
     }
 
-    private void initView() {
-        LayoutInflater inflater = LayoutInflater.from(activity);
-        container = (LinearLayout) inflater
-                .inflate(R.layout.form_subform_layout, parent, false);
+    private int getInsertIndex() {
+        return parent.getChildCount() - NUM_CHILD_VIEWS;
+    }
 
-        fieldList = (RecyclerView) container.findViewById(R.id.field_list);
-        addSubForm = (Button) container.findViewById(R.id.add);
-        deleteSubForm = (Button) container.findViewById(R.id.delete);
+    private void initDeleteBtn(ViewGroup container) {
+        Button deleteBtn = (Button) container.findViewById(R.id.delete);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parent.removeView((View) v.getParent());
+            }
+        });
+    }
+
+    private void initFieldList(ViewGroup container) {
+        RecyclerView fieldList = (RecyclerView) container.findViewById(R.id.field_list);
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(activity);
+        layout.setAutoMeasureEnabled(true);
+        fieldList.setLayoutManager(layout);
+        CaseRegisterAdapter adapter = new CaseRegisterAdapter(activity, fields);
+        fieldList.setAdapter(adapter);
     }
 }
