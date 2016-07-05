feca6be38e7d94d901098d7dd340a3746abdd1a3
CHEN Yang <yangchen@thoughtworks.com>
2016-07-05 12:50:19 +0800

    feature/#64--add sync progress dialog & base logic

--- app/src/main/java/org/unicef/rapidreg/sync/SyncPresenter.java
+++ app/src/main/java/org/unicef/rapidreg/sync/SyncPresenter.java
@@ -1,6 +1,66 @@
 package org.unicef.rapidreg.sync;
 
+import android.content.Context;
+
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 
 public class SyncPresenter extends MvpBasePresenter<SyncView> {
+
+    private Context context;
+
+    public SyncPresenter() {}
+
+    public SyncPresenter(Context context) {
+        this.context = context;
+    }
+
+    public void doSync() {
+        if (isViewAttached()) {
+            getView().showSyncProgressDialog();
+            try {
+                startUpLoadCases();
+                startDownLoadCaseForms();
+                startDownLoadCases();
+                doSyncSuccessAction();
+            } catch (Exception e) {
+                doSyncFailureAction();
+            }
+        }
+    }
+
+    public void doAttemptCancelSync() {
+        getView().showSyncCancelConfirmDialog();
+    }
+
+    public void doCancelSync() {
+
+    }
+
+    private void startUpLoadCases() {
+
+    }
+
+    private void startDownLoadCaseForms() {
+
+    }
+
+    private void startDownLoadCases() {
+
+    }
+
+    private void doSyncSuccessAction() {
+        updateDataViews();
+        getView().showSyncSuccessMessage();
+        getView().hideSyncProgressDialog();
+    }
+
+    private void doSyncFailureAction() {
+        updateDataViews();
+        getView().showSyncErrorMessage();
+        getView().hideSyncProgressDialog();
+    }
+
+    private void updateDataViews() {
+
+    }
 }
--- app/src/main/java/org/unicef/rapidreg/sync/SyncView.java
+++ app/src/main/java/org/unicef/rapidreg/sync/SyncView.java
@@ -3,4 +3,16 @@ package org.unicef.rapidreg.sync;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 public interface SyncView extends MvpView {
+
+    public void showSyncProgressDialog();
+
+    public void hideSyncProgressDialog();
+
+    public void showSyncCancelConfirmDialog();
+
+    public void showSyncErrorMessage();
+
+    public void showSyncSuccessMessage();
+
+    public void setDataViews(String syncDate, String hasSyncAmount, String notSyncAmount);
 }
