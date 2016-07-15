977992ae2af274473da3352a1da0b30ec0ff7bdb
bfeng <fungbo@126.com>
2016-07-15 16:45:41 +0800

    [#X] - collapse floating menu firstly when clicking anywhere if the floating menu is expanded

--- app/src/main/java/org/unicef/rapidreg/base/RecordListFragment.java
+++ app/src/main/java/org/unicef/rapidreg/base/RecordListFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -114,6 +115,29 @@ public abstract class RecordListFragment extends MvpFragment<RecordListView, Rec
         adapter.toggleViews(isShow);
     }
 
+    protected void initListContainer(final RecordListAdapter adapter) {
+        listContainer.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                if (floatingMenu.isExpanded()) {
+                    floatingMenu.collapse();
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+
     private void setListAlpha(float value) {
         container.setAlpha(value);
     }
@@ -134,8 +158,6 @@ public abstract class RecordListFragment extends MvpFragment<RecordListView, Rec
     }
 
     protected abstract void initOrderSpinner(final RecordListAdapter adapter);
-
-    protected abstract void initListContainer(final RecordListAdapter adapter);
 
     public enum SpinnerState {
         AGE_ASC(R.drawable.age_up, "Age ascending order", "Age"),
