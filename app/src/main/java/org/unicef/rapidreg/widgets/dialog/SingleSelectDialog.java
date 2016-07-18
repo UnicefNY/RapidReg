65943091ce9f97c9ab7d872243269c89ad15d2ee
Zhe Chen <zhechen@thoughtworks.com>
2016-07-18 17:06:28 +0800

    add filter for selectdialog

--- app/src/main/java/org/unicef/rapidreg/widgets/dialog/SingleSelectDialog.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/dialog/SingleSelectDialog.java
@@ -2,18 +2,30 @@ package org.unicef.rapidreg.widgets.dialog;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
+import org.unicef.rapidreg.R;
 import org.unicef.rapidreg.forms.Field;
 import org.unicef.rapidreg.service.cache.ItemValuesMap;
+import org.unicef.rapidreg.widgets.viewholder.GenericViewHolder;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class SingleSelectDialog extends BaseDialog {
 
     private String result;
     private String[] optionItems;
+
+    SearchAbleDialog dialog;
 
     public SingleSelectDialog(Context context, Field field,
                               ItemValuesMap itemValues, TextView resultView, ViewSwitcher viewSwitcher) {
@@ -30,13 +42,52 @@ public class SingleSelectDialog extends BaseDialog {
         selectIndex = selectIndex == -1 ? 0 : selectIndex;
         result = optionItems[selectIndex];
 
-        getBuilder().setSingleChoiceItems(optionItems,
-                selectIndex, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        result = optionItems[which];
-                    }
-                });
+        dialog = new SearchAbleDialog(context,field.getDisplayName().get(Locale.getDefault().getLanguage()) ,optionItems);
+
+        dialog.setOnClick(new SearchAbleDialog.SearchAbleDialogOnClickListener() {
+            @Override
+            public void onClick(String result) {
+                SingleSelectDialog.this.result = result;
+                Log.d("asdf", result);
+            }
+        });
+
+        dialog.setCancelButton(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.setOkButton(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getResult() != null && !TextUtils.isEmpty(getResult().toString())) {
+                    viewSwitcher.setDisplayedChild(GenericViewHolder.FORM_HAS_ANSWER_STATE);
+                } else {
+                    viewSwitcher.setDisplayedChild(GenericViewHolder.FORM_NO_ANSWER_STATE);
+                }
+                resultView.setText(getResult() == null ? null : getResult().toString());
+
+                if (isSubFormField()) {
+                    String language = Locale.getDefault().getLanguage();
+                    itemValues.addChildrenItemForParent(field.getParent(), field.getIndex(),
+                            field.getDisplayName().get(language), getResult());
+                } else {
+                    itemValues.addItem(field.getName(), getResult());
+                }
+
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+    @Override
+    public void show() {
+        initView();
+
+        dialog.show();
     }
 
     @Override
