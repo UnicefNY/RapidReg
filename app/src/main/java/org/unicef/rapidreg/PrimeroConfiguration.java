c234c9bbc454f59eee7ba18c326863425a645888
shane51 <suse51@163.com>
2016-07-18 14:06:26 +0800

    [Bugfix] Use internal storage to store image

--- app/src/main/java/org/unicef/rapidreg/PrimeroConfiguration.java
+++ app/src/main/java/org/unicef/rapidreg/PrimeroConfiguration.java
@@ -1,6 +1,10 @@
 package org.unicef.rapidreg;
 
 public class PrimeroConfiguration {
+    public static final float PHOTO_MAX_WIDTH = 1080;
+    public static final float PHOTO_MAX_HEIGHT = 1920;
+    public static final int PHOTO_MAX_SIZE_KB = 1000;
+    public static final int PHOTO_THUMBNAIL_SIZE = 210;
     //    public static final String API_BASE_URL = "http://10.29.3.184:3000";
     private static String apiBaseUrl = "https://10.29.3.184:8443";
 
