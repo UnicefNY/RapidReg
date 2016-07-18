30b21852f3db2dac687b503d05f80be0deabb195
shane51 <suse51@163.com>
2016-07-18 16:04:59 +0800

    [Bugfix]  Force mark for mobile Uneditable

--- app/src/main/java/org/unicef/rapidreg/widgets/viewholder/BaseViewHolder.java
+++ app/src/main/java/org/unicef/rapidreg/widgets/viewholder/BaseViewHolder.java
@@ -33,6 +33,9 @@ public abstract class BaseViewHolder<T> extends RecyclerView.ViewHolder {
     }
 
     protected boolean isEditable(Field field) {
+        if (field.isMarkForMobileField()) {
+            return false;
+        }
         return field.isEditable();
     }
 
