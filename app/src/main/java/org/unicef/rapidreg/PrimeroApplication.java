69f10fab73af052302241cbe15bb6711c5ecfc05
shane51 <suse51@163.com>
2016-07-18 11:02:01 +0800

    [Bugfix] use internal storage to store audio file

--- app/src/main/java/org/unicef/rapidreg/PrimeroApplication.java
+++ app/src/main/java/org/unicef/rapidreg/PrimeroApplication.java
@@ -30,6 +30,8 @@ public class PrimeroApplication extends Application {
             Stetho.initializeWithDefaults(context);
         }
 
+        PrimeroConfiguration.setInternalFilePath(context.getFilesDir().getPath() );
+
         initDB();
     }
 
