c3c2ec7185f6932fedd92bff6305c0dd52eb18a3
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-07-22 10:00:59 +0800

    [#] resolve compile error

--- app/src/main/java/org/unicef/rapidreg/sync/SyncFragment.java
+++ app/src/main/java/org/unicef/rapidreg/sync/SyncFragment.java
@@ -3,7 +3,9 @@ package org.unicef.rapidreg.sync;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.hannesdorfmann.mosby.mvp.MvpFragment;
 
 import org.unicef.rapidreg.R;
@@ -47,7 +50,17 @@ public class SyncFragment extends MvpFragment<SyncView, SyncPresenter> implement
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sync, container, false);
         ButterKnife.bind(this, view);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String syncStatisticData = sharedPreferences.getString("syncStatisticData",new Gson().toJson(new SyncStatisticData()));
+        SyncStatisticData syncData = new Gson().fromJson(syncStatisticData, SyncStatisticData.class);
+        lastSyncTime.setText(syncData.getLastSyncData());
+        countOfLastSync.setText(syncData.getSyncedNumberAsString());
+        countOfNotSync.setText(syncData.getNotSyncedNumberAsString());
     }
 
     @Override
