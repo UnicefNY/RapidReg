93b941ea488e581e9f7b83dfe375707603c33d1e
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-08-01 16:18:21 +0800

    [#Refactor] refactor the login msg

--- app/src/main/java/org/unicef/rapidreg/network/HttpStatusCodeHandler.java
+++ app/src/main/java/org/unicef/rapidreg/network/HttpStatusCodeHandler.java
@@ -2,22 +2,19 @@ package org.unicef.rapidreg.network;
 
 import android.util.Log;
 
-public class HttpStatusCodeHandler {
-    public static final String NOT_FIND_CODE_MESSAGE = "Not find status code!";
-    public static final String UNAUTHORIZED_MESSAGE = "Sorry, incorrect username or password.";
-    public static final String LOGIN_SUCCESS_MESSAGE = "Login successful";
-    public static final String SERVICE_UNAVAILABLE_MESSAGE = "Service Unavailable!";
+import org.unicef.rapidreg.R;
 
-    public static String getHttpStatusMessage(int statusCode) {
+public class HttpStatusCodeHandler {
+    public static int getHttpStatusMessage(int statusCode) {
         Log.e("TAG", "getHttpStatusMessage: " + statusCode + "");
         switch (statusCode) {
             case 401:
-                return UNAUTHORIZED_MESSAGE;
+                return R.string.login_unauthorized_message;
             case 503:
-                return SERVICE_UNAVAILABLE_MESSAGE;
+                return R.string.login_service_unavailable_message;
             default:
                 break;
         }
-        return NOT_FIND_CODE_MESSAGE;
+        return R.string.login_not_find_code_message;
     }
 }
