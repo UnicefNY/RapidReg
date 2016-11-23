e7d167581b929242644e38d86f0c2c2ce418557b
Nanchen Yang <ncyang@thoughtworks.com>
2016-11-23 10:45:45 +0800

    [Nanchen] [refactor] moved service from fragment to presenter.

--- app/src/main/java/org/unicef/rapidreg/base/RecordListPresenter.java
+++ app/src/main/java/org/unicef/rapidreg/base/RecordListPresenter.java
@@ -4,11 +4,25 @@ import android.content.Context;
 
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 
+import org.unicef.rapidreg.base.RecordListFragment.SpinnerState;
 import org.unicef.rapidreg.childcase.CaseListAdapter;
+import org.unicef.rapidreg.model.Case;
 import org.unicef.rapidreg.model.RecordModel;
+import org.unicef.rapidreg.service.CaseFormService;
+import org.unicef.rapidreg.service.CaseService;
+import org.unicef.rapidreg.service.RecordService;
 import org.unicef.rapidreg.tracing.TracingListAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecordListPresenter extends MvpBasePresenter<RecordListView> {
+
+    CaseService caseService = CaseService.getInstance();
+
+    CaseFormService caseFormService;
+
+    RecordService recordService;
 
     private int type;
 
@@ -24,5 +38,32 @@ public class RecordListPresenter extends MvpBasePresenter<RecordListView> {
                 getView().initView(new TracingListAdapter(context));
             }
         }
+    }
+
+    public List<Case> getCases() {
+        return caseService.getAll();
+    }
+
+    public List<Long> getCasesByFilter(SpinnerState spinnerState) {
+        switch (spinnerState) {
+            case AGE_ASC:
+                return caseService.getAllOrderByAgeASC();
+            case AGE_DES:
+                return caseService.getAllOrderByAgeDES();
+            case REG_DATE_ASC:
+                return caseService.getAllOrderByDateASC();
+            case REG_DATE_DES:
+                return caseService.getAllOrderByDateDES();
+            default:
+                return new ArrayList<>();
+        }
+    }
+
+    public void clearAudioFile() {
+        RecordService.clearAudioFile();
+    }
+
+    public boolean isFormReady() {
+        return CaseFormService.getInstance().isFormReady();
     }
 }
