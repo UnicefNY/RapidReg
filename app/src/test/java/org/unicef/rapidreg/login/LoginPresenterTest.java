de2e3bc3ca7531c6e5a9a148fe0c7139448679f8
Nanchen Yang <ncyang@thoughtworks.com>
2016-12-07 16:39:45 +0800

    [Nanchen][#98] fixed LoginPresenter test.

--- app/src/test/java/org/unicef/rapidreg/login/LoginPresenterTest.java
+++ app/src/test/java/org/unicef/rapidreg/login/LoginPresenterTest.java
@@ -1,39 +1,62 @@
 package org.unicef.rapidreg.login;
 
+import android.content.Context;
 import android.os.Build;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.unicef.rapidreg.BuildConfig;
+import org.unicef.rapidreg.network.AuthService;
+import org.unicef.rapidreg.service.UserService;
 
 import javax.inject.Inject;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.JELLY_BEAN)
 public class LoginPresenterTest {
-    @Inject
-    LoginPresenter loginPresenter;
+
+    private LoginPresenter loginPresenter;
+
+    @Mock
+    private Context context;
+
+    @Mock
     private LoginView loginView;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private AuthService authService;
 
     @Before
     public void setUp() throws Exception {
+        initMocks(this);
 
-        loginView = mock(LoginView.class);
+        loginPresenter = new LoginPresenter(context, userService, authService);
         loginPresenter.attachView(loginView);
     }
 
-    @Ignore
     @Test
     public void should_show_error_when_validate_empty_password_and_passord() {
+
+        when(userService.isNameValid("")).thenReturn(false);
+        when(userService.isPasswordValid("")).thenReturn(false);
+        when(userService.isUrlValid("http://10.29.3.184:3000")).thenReturn(true);
+
         boolean valid = loginPresenter.validate(RuntimeEnvironment.application, "", "", "http://10.29.3.184:3000");
         verify(loginView).showUserNameError("Enter a valid username!");
         verify(loginView).showPasswordError("Enter a valid password!");
@@ -41,9 +64,13 @@ public class LoginPresenterTest {
     }
 
 
-    @Ignore
     @Test
     public void should_show_error_when_invalid_user_format() {
+
+        when(userService.isNameValid("pri mero")).thenReturn(false);
+        when(userService.isPasswordValid("password")).thenReturn(true);
+        when(userService.isUrlValid("http://10.29.3.184:3000")).thenReturn(true);
+
         boolean valid = loginPresenter.validate(RuntimeEnvironment.application, "pri mero", "password", "http://10.29.3.184:3000");
         verify(loginView).showUserNameError("Enter a valid username!");
         assertEquals(valid, false);
