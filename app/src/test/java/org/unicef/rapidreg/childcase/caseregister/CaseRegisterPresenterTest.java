8feccbab9dc42dc1e5914521fd6b37b4cf90e403
Nanchen Yang <ncyang@thoughtworks.com>
2016-12-16 10:06:21 +0800

    [Nanchen][#133] added CaseRegisterPresenter Test.

--- app/src/test/java/org/unicef/rapidreg/childcase/caseregister/CaseRegisterPresenterTest.java
+++ app/src/test/java/org/unicef/rapidreg/childcase/caseregister/CaseRegisterPresenterTest.java
@@ -1,5 +1,7 @@
 package org.unicef.rapidreg.childcase.caseregister;
 
+import com.raizlabs.android.dbflow.data.Blob;
+
 import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,21 +27,25 @@ import org.unicef.rapidreg.service.RecordService;
 import org.unicef.rapidreg.service.cache.ItemValues;
 import org.unicef.rapidreg.service.cache.ItemValuesMap;
 
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyMap;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.unicef.rapidreg.childcase.caseregister.CaseRegisterPresenter.MODULE_CASE_CP;
+import static org.unicef.rapidreg.service.cache.ItemValues.RecordProfile.ID;
+import static org.unicef.rapidreg.service.cache.ItemValues.RecordProfile.ID_NORMAL_STATE;
+import static org.unicef.rapidreg.service.cache.ItemValues.RecordProfile.REGISTRATION_DATE;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({RecordService.class})
@@ -56,6 +62,20 @@ public class CaseRegisterPresenterTest {
 
     @InjectMocks
     private CaseRegisterPresenter caseRegisterPresenter;
+
+    private static final String CASE_JSON =
+            "{" +
+                    "\"safety_plan_main_concern\":\"kkkk\"," +
+                    "\"safety_plan_resources_economic\":\"hjvgh\"," +
+                    "\"consent_for_services\":true," +
+                    "\"module_id\":\"primeromodule-gbv\"," +
+                    "\"case_id_display\":\"1cb7a98\"," +
+                    "\"case_id\":\"6cd8cee7-3dea-4417-ba05-9b0b81cb7a98\"," +
+                    "\"owned_by\":\"primero\"," +
+                    "\"created_by\":\"primero\"," +
+                    "\"previously_owned_by\":\"primero\"," +
+                    "\"registration_date\":\"15/12/2016\"" +
+            "}";
 
     @Before
     public void setUp() throws Exception {
@@ -120,4 +140,42 @@ public class CaseRegisterPresenterTest {
         verify(callback).onRequiredFieldNotFilled();
     }
 
+
+    @Test
+    public void should_return_item_values_by_record_id() throws Exception {
+        Date date = new Date();
+        date.setYear(2016);
+        date.setMonth(12);
+        date.setDate(15);
+
+        Case caseItem = new Case();
+        caseItem.setContent(new Blob(CASE_JSON.getBytes()));
+        caseItem.setId(123L);
+        caseItem.setUniqueId("6cd8cee7-3dea-4417-ba05-9b0b81cb7a98");
+        caseItem.setRegistrationDate(date);
+
+        when(caseService.getById(123L)).thenReturn(caseItem);
+
+        String shortUUID = "uuid";
+        PowerMockito.mockStatic(RecordService.class);
+        when(RecordService.getShortUUID(caseItem.getUniqueId())).thenReturn(shortUUID);
+
+        ItemValuesMap actual = caseRegisterPresenter.getItemValuesByRecordId(123L);
+
+        assertThat("Should have same size of items", actual.getValues().size(), is(13));
+        assertThat("safety_plan_main_concern should be right", actual.getAsString("safety_plan_main_concern"), is("kkkk"));
+        assertThat("safety_plan_resources_economic should be right", actual.getAsString("safety_plan_resources_economic"), is("hjvgh"));
+        assertThat("consent_for_services should be right", actual.getAsBoolean("consent_for_services"), is(true));
+        assertThat("module_id should be right", actual.getAsString("module_id"), is("primeromodule-gbv"));
+        assertThat("case_id_display should be right", actual.getAsString("case_id_display"), is("1cb7a98"));
+        assertThat("case_id should be right", actual.getAsString("case_id"), is("6cd8cee7-3dea-4417-ba05-9b0b81cb7a98"));
+        assertThat("owned_by should be right", actual.getAsString("owned_by"), is("primero"));
+        assertThat("created_by should be right", actual.getAsString("created_by"), is("primero"));
+        assertThat("previously_owned_by should be right", actual.getAsString("previously_owned_by"), is("primero"));
+        assertThat("_id_normal_state should be right", actual.getAsString("_id_normal_state"), is(shortUUID));
+        assertThat("_id should be right", actual.getAsLong("_id"), is(123L));
+        assertThat("_registration_date should be right", actual.getAsString("_registration_date"),
+                is(SimpleDateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US).format(date)));
+        assertThat("registration_date should be right", actual.getAsString("registration_date"), is("15/12/2016"));
+    }
 }
