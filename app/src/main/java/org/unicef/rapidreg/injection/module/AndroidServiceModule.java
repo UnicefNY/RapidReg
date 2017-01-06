12ec4d2df529372b0866999dfcbac2709f449d06
Nanchen Yang <ncyang@thoughtworks.com>
2017-01-06 22:32:34 +0800

    [Nanchen][#145] added Android Service injection module.

---
+++ app/src/main/java/org/unicef/rapidreg/injection/module/AndroidServiceModule.java
@@ -0,0 +1,28 @@
+package org.unicef.rapidreg.injection.module;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.telephony.TelephonyManager;
+
+import org.unicef.rapidreg.injection.ApplicationContext;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class AndroidServiceModule {
+
+    @Provides
+    @Singleton
+    TelephonyManager provideTelephonyManager(@ApplicationContext Context context) {
+        return (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    }
+
+    @Provides
+    @Singleton
+    ConnectivityManager provideConnectivityManager(@ApplicationContext Context context) {
+        return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+}
