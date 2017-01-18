96de65027da8036a6a93b07243bc078ebc34cc17
Nanchen Yang <ncyang@thoughtworks.com>
2017-01-18 16:04:13 +0800

    [Nanchen][#105] modified value of registration date of incident.

--- app/src/main/java/org/unicef/rapidreg/service/RecordService.java
+++ app/src/main/java/org/unicef/rapidreg/service/RecordService.java
@@ -32,6 +32,7 @@ public class RecordService {
     public static final String OTHER_NAME = "name_other";
     public static final String CAREGIVER_NAME = "name_caregiver";
     public static final String REGISTRATION_DATE = "registration_date";
+    public static final String DATE_OF_INTERVIEW = "date_of_first_report";
     public static final String CASEWORKER_CODE = "owned_by";
     public static final String RECORD_CREATED_BY = "created_by";
     public static final String PREVIOUS_OWNER = "previously_owned_by";
