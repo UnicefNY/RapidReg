e77946dfd224a00ac63679f23729d436baa982fd
sunny <137126846@qq.com>
2017-01-22 10:56:45 +0800

    [#159] change GBVSyncPresenter

---
+++ app/src/main/java/org/unicef/rapidreg/service/SyncIncidentService.java
@@ -0,0 +1,16 @@
+package org.unicef.rapidreg.service;
+
+import com.google.gson.JsonElement;
+
+import org.unicef.rapidreg.model.Incident;
+
+import retrofit2.Response;
+import rx.Observable;
+
+public interface SyncIncidentService {
+    Response<JsonElement> uploadJsonProfile(Incident qitem);
+
+    Observable<Response<JsonElement>> getIds(String lastUpdate, boolean isMobile);
+
+    Observable<Response<JsonElement>> get(String id, String locale, boolean isMobile);
+}
