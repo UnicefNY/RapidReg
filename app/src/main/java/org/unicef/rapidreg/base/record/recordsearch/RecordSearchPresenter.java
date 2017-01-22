570dbe5403d7976963d8fe5de84ab40e2769f67e
Nanchen Yang <ncyang@thoughtworks.com>
2017-01-22 14:16:33 +0800

    [Nanchen][#114] modified new search function to adapt different kind of search condition.

--- app/src/main/java/org/unicef/rapidreg/base/record/recordsearch/RecordSearchPresenter.java
+++ app/src/main/java/org/unicef/rapidreg/base/record/recordsearch/RecordSearchPresenter.java
@@ -7,16 +7,22 @@ import org.unicef.rapidreg.base.record.recordlist.RecordListView;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class RecordSearchPresenter extends MvpBasePresenter<RecordListView> {
 
-    protected static final String ID = "id";
-    protected static final String NAME = "name";
-    protected static final String AGE_FROM = "age_from";
-    protected static final String AGE_TO = "age_to";
-    protected static final String CAREGIVER = "caregiver";
-    protected static final String REGISTRATION_DATE = "registration_date";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String AGE_FROM = "age_from";
+    public static final String AGE_TO = "age_to";
+    public static final String CAREGIVER = "caregiver";
+    public static final String REGISTRATION_DATE = "registration_date";
+    public static final String DATE_OF_INQUIRY = "date_of_inquiry";
+    public static final String LOCATION = "location";
+    public static final String SURVIVOR_CODE = "survivor_code";
+    public static final String TYPE_OF_VIOLENCE = "type_of_violence";
 
     protected Date getDate(String value) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -28,10 +34,5 @@ public abstract class RecordSearchPresenter extends MvpBasePresenter<RecordListV
         return null;
     }
 
-    protected abstract List<Long> getSearchResult(String shortId,
-                                                  String name,
-                                                  int ageFrom,
-                                                  int ageTo,
-                                                  String caregiver,
-                                                  String registrationDate);
+    protected abstract List<Long> getSearchResult(Map<String, String> searchConditions);
 }
