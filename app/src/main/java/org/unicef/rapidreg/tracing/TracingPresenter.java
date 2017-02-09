6fe2c739e9010718c36da5be92c4450ff4b673c4
Nanchen Yang <ncyang@thoughtworks.com>
2017-02-09 17:59:44 +0800

    [Nanchen][#feature] removed load tracing form in tracing presenter.

--- app/src/main/java/org/unicef/rapidreg/tracing/TracingPresenter.java
+++ app/src/main/java/org/unicef/rapidreg/tracing/TracingPresenter.java
@@ -13,11 +13,9 @@ import javax.inject.Inject;
 
 public class TracingPresenter extends RecordPresenter {
     private TracingFormService tracingFormService;
-    private FormRemoteService formRemoteService;
 
     @Inject
-    public TracingPresenter(FormRemoteService authService, TracingFormService tracingFormService) {
-        this.formRemoteService = authService;
+    public TracingPresenter(TracingFormService tracingFormService) {
         this.tracingFormService = tracingFormService;
     }
 
@@ -25,20 +23,5 @@ public class TracingPresenter extends RecordPresenter {
     public void saveForm(RecordForm recordForm, String moduleId) {
         TracingForm tracingForm = new TracingForm(new Blob(gson.toJson(recordForm).getBytes()));
         tracingFormService.saveOrUpdate(tracingForm);
-    }
-
-    public void loadTracingForm() {
-        formRemoteService.getTracingForm(PrimeroAppConfiguration.getCookie(),
-                PrimeroAppConfiguration.getDefaultLanguage(), true, PrimeroAppConfiguration.PARENT_TRACING_REQUEST,
-                PrimeroAppConfiguration.MODULE_ID_CP)
-                .subscribe(tracingTemplateForm -> {
-                    saveForm(tracingTemplateForm, PrimeroAppConfiguration.MODULE_ID_CP);
-                    setFormSyncFail(false);
-                }, throwable -> {
-                    if (isViewAttached()) {
-                        getView().promoteSyncFormsError();
-                    }
-                    setFormSyncFail(true);
-                });
     }
 }
