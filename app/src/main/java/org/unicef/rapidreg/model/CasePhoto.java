a07442e5be78149b0421e098dbb7b4b8bc4a1dc0
Nanchen Yang <ncyang@thoughtworks.com>
2017-03-10 16:08:20 +0800

    [#172] upgrade dbflow version to support Android 7.0

--- app/src/main/java/org/unicef/rapidreg/model/CasePhoto.java
+++ app/src/main/java/org/unicef/rapidreg/model/CasePhoto.java
@@ -3,7 +3,6 @@ package org.unicef.rapidreg.model;
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.Index;
 import com.raizlabs.android.dbflow.annotation.IndexGroup;
-import com.raizlabs.android.dbflow.annotation.ModelContainer;
 import com.raizlabs.android.dbflow.annotation.Table;
 
 import org.unicef.rapidreg.PrimeroDatabaseConfiguration;
@@ -11,9 +10,7 @@ import org.unicef.rapidreg.PrimeroDatabaseConfiguration;
 @Table(database = PrimeroDatabaseConfiguration.class, indexGroups = {
         @IndexGroup(number = 1, name = "indexCaseId")
 })
-@ModelContainer
 public class CasePhoto extends RecordPhoto {
-
     @Index(indexGroups = 1)
     @Column
     long caseId;
--- app/src/main/java/org/unicef/rapidreg/model/TracingPhoto.java
+++ app/src/main/java/org/unicef/rapidreg/model/TracingPhoto.java
@@ -3,7 +3,6 @@ package org.unicef.rapidreg.model;
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.Index;
 import com.raizlabs.android.dbflow.annotation.IndexGroup;
-import com.raizlabs.android.dbflow.annotation.ModelContainer;
 import com.raizlabs.android.dbflow.annotation.Table;
 
 import org.unicef.rapidreg.PrimeroDatabaseConfiguration;
@@ -11,7 +10,6 @@ import org.unicef.rapidreg.PrimeroDatabaseConfiguration;
 @Table(database = PrimeroDatabaseConfiguration.class, indexGroups = {
         @IndexGroup(number = 1, name = "indexTracingId")
 })
-@ModelContainer
 public class TracingPhoto extends RecordPhoto {
 
     @Index(indexGroups = 1)
